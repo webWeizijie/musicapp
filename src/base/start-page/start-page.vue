c391911a5ae2fcf51fd2dd82bf940012b6932bee
wzj <974960593@qq.com>
2017-10-27 16:52:44 +0800

    startPage

---
+++ src/base/start-page/start-page.vue
@@ -0,0 +1,76 @@
+<template>
+	<transition name="fadeOut">
+		<div class="star-page">
+			<div class="time" @click="router">
+				跳过 {{time}}s
+			</div>
+		</div>
+	</transition>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				time:8,
+			}
+		},
+		activated() {
+			this.time= 3;
+		},
+		methods:{
+			router(){
+				this.$router.push({
+					path:'/recommend'
+				})
+				this.time = 0;
+			},
+			changeTime(){
+				setTimeout(() => {
+					this.time--
+				}, 1000)
+			}
+		},
+		watch:{
+			time(){
+				if(this.time > 0){
+					this.changeTime()
+				}else{
+					this.$router.push({
+						path:'/recommend'
+					})
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.star-page {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+		background: #222;
+	}
+	
+	.fadeOut-leave-active {
+		transition: all 0.5s ease;
+	}
+	
+	.fadeOut-leave-to {
+		opacity: 0;
+		transform: translate3d(80%, 0, 0);
+	}
+	
+	.time {
+		opacity: 0.7;
+		position: absolute;
+		right: 14px;
+		top: 18px;
+		font-size: 14px;
+		border: 1px solid rgba(255, 255, 255, 0.5);
+		padding: 5px 11px;
+		border-radius: 20px;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,67 +1,68 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-//import ranking from 'components/ranking/ranking'
-//import singer from 'components/singer/singer'
-//import recommend from 'components/recommend/recommend'
-//import search from 'components/search/search'
-//import singerDetail from 'components/singer-detail/singer-detail'
-//import disc from 'components/disc/disc'
-//import rankList from 'components/rank-list/rank-list'
-//import userCenter from 'components/user-center/user-center'
+import ranking from 'components/ranking/ranking'
+import singer from 'components/singer/singer'
+import recommend from 'components/recommend/recommend'
+import search from 'components/search/search'
+import singerDetail from 'components/singer-detail/singer-detail'
+import disc from 'components/disc/disc'
+import rankList from 'components/rank-list/rank-list'
+import userCenter from 'components/user-center/user-center'
+import startPage from 'base/start-page/start-page'
 Vue.use(Router)
 
-const recommend = (resolve) =>{
-	 import('components/recommend/recommend').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const singer = (resolve) =>{
-	 import('components/singer/singer').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const ranking = (resolve) =>{
-	 import('components/ranking/ranking').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const search = (resolve) =>{
-	 import('components/search/search').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const singerDetail = (resolve) =>{
-	 import('components/singer-detail/singer-detail').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const disc = (resolve) =>{
-	 import('components/disc/disc').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const rankList = (resolve) =>{
-	 import('components/rank-list/rank-list').then((module)=>{
-	 		resolve(module)
-	 })
-}
-
-const userCenter = (resolve) =>{
-	 import('components/user-center/user-center').then((module)=>{
-	 		resolve(module)
-	 })
-}
+//const recommend = (resolve) =>{
+//	 import('components/recommend/recommend').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const singer = (resolve) =>{
+//	 import('components/singer/singer').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const ranking = (resolve) =>{
+//	 import('components/ranking/ranking').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const search = (resolve) =>{
+//	 import('components/search/search').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const singerDetail = (resolve) =>{
+//	 import('components/singer-detail/singer-detail').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const disc = (resolve) =>{
+//	 import('components/disc/disc').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const rankList = (resolve) =>{
+//	 import('components/rank-list/rank-list').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
+//
+//const userCenter = (resolve) =>{
+//	 import('components/user-center/user-center').then((module)=>{
+//	 		resolve(module)
+//	 })
+//}
 export default new Router({
   routes: [
     {
       path: '/',
-      redirect:'/recommend'
+      component:startPage,
     },
     {
     	path:'/recommend',
